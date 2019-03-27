aa746d4576a22d79df299e69306457187b6a532d
Tomek F <user2000wp@wp.pl>
2019-03-27 14:22:32 +0100

    Drugi commit próbny.

--- KursyCSharp_ZbiorWiedzy/Program.cs
+++ KursyCSharp_ZbiorWiedzy/Program.cs
@@ -6,7 +6,15 @@ namespace KursyCSharp_ZbiorWiedzy
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            // zamiast if można stosować operator trójargumentowy - przypisze 0 jeśli true lub age gdy false
+            int age = 0;
+            var result = age == 0 ? 0 : age;
+
+            // jeśli hello będzie null to name będzie Dawid
+            string hello = null;
+            var name = hello ?? "Dawid";
+
+
         }
     }
 }
