e30908baaddda81fff21c5d33ded9bbe042f8a64
Tomek F <user2000wp@wp.pl>
2019-03-28 18:23:12 +0100

    Po lekcji 3.18.

--- KursyCSharp_ZbiorWiedzy/Program.cs
+++ KursyCSharp_ZbiorWiedzy/Program.cs
@@ -2,10 +2,43 @@
 
 namespace KursyCSharp_ZbiorWiedzy
 {
+    class Samochod
+    {
+        // get set - zapis do pól klasy bez konieczności tworzenia metod get, set
+        public string marka { get; set; }
+        public int rok_produkcji { get; set; }
+
+        public Samochod(string marka, int rok_produkcji)
+        {
+            this.marka = marka;
+            this.rok_produkcji = rok_produkcji;
+        }
+    }
+
     class Program
     {
+        // do metody przez argument przekazywana jest kopia zmiennej! (odc. 3.17)
+        static int PrzypiszNowaWartosc(int nowaWart)
+        {
+            nowaWart = 12345;
+            return nowaWart;
+        }
+
         static void Main(string[] args)
         {
+            // do metody przez argument przekazywana jest kopia zmiennej! (odc. 3.17)
+            int staraWartosc = 1;
+            PrzypiszNowaWartosc(staraWartosc);
+            Console.WriteLine(staraWartosc); //!!!
+            Console.WriteLine(PrzypiszNowaWartosc(staraWartosc));
+            Console.ReadLine();
+
+            // użycie get, set
+            Samochod skoda = new Samochod("Skoda", 2018);
+            Console.WriteLine(skoda.marka);
+            Console.WriteLine(skoda.rok_produkcji);
+            Console.ReadLine();
+
             // zamiast if można stosować operator trójargumentowy - przypisze 0 jeśli true lub age gdy false
             int age = 0;
             var result = age == 0 ? 0 : age;
@@ -14,7 +47,10 @@ namespace KursyCSharp_ZbiorWiedzy
             string hello = null;
             var name = hello ?? "Dawid";
 
-
+            //porównanie stringów 
+            string name1 = "haha";
+            string name2 = "hehe";
+            bool areEqual = name1.Equals(name2, StringComparison.CurrentCultureIgnoreCase);
         }
     }
 }
